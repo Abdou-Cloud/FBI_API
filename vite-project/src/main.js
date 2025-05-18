8378f1759d4121df9d2c3edefb53f9584c932de6
Abdou-Cloud <ab.abdou1800@gmail.com>
2025-05-18 13:56:57 +0200

    data opgehaald van API
    ik nam 50 omdat ik niet te veel data wou hebben en er een minimum van 20 was volgens de opdracht

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -3,3 +3,10 @@ import './style.css'
 const zoekInput = document.getElementById("searchInput");
 const filterType = document.getElementById("filterOptions");
 const container = document.getElementById("resultsContainer");
+
+
+async function dataOphalen() {
+    const res = await fetch("https://api.fbi.gov/wanted/v1/list?pageSize=50");
+    const data = await res.json();
+    return data.items;
+  }
